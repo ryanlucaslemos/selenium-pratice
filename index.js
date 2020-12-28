24534ca180da24fb19fd44e27af7ef51d0371f3a
Ryan Lemos <ryanlucas.lemos@gmail.com>
2020-12-28 14:30:13 -0300

    Comenta o código inicial

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
 
 /**
- * To run it execute: node index
+ * Para rodar o teste execute-o via: node index
  */
 async function clickingTest() {
   const driver = new Builder().forBrowser('chrome').build();
 
   try {
+    // Acessa via método get a página do google
     await driver.get('https://www.google.com.br/');
+
+    /* 
+      Busca na página um elemento pelo atributo name do html(findElement). 
+      Depois preenche o input com a palavra selenium e pressiona enter (sendKeys)
+    */
     await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
-    // await driver.close();
+    // await driver.close(); fecha o navegador
   } catch (e) {
     console.log(e);
   }
